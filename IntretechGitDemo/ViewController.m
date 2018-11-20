0e75c2a338d5a41f4186f8dafae8d9c154150340
mohamedaslam28 <mohamedaslam28@gmail.com>
2018-11-20 19:56:47 +0800

    Initial commit to master

---
+++ IntretechGitDemo/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  IntretechGitDemo
+//
+//  Created by intretech on 2018/11/20.
+//  Copyright © 2018 intretech. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    //dfghfh
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
